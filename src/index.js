16f5ed0d5721d072fc37a9f4ce48914ac9f90c36
RaffaeleMassa <76770497+RaffaeleMassa@users.noreply.github.com>
2021-03-22 17:13:30 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
+import axios from "axios"
 
+async function callLambdaFunction() {
+ 
+  const response = await axios.get("/.netlify/functions/getPollution")
+
+  console.log(response) 
+} 
 
 callLambdaFunction()
 
