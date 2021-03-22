ba2450ccfc27b31da61b67d1301903d384f494cd
RaffaeleMassa <76770497+RaffaeleMassa@users.noreply.github.com>
2021-03-22 16:44:58 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin")
           {
             test: /\.css$/,
             use: ["style-loader", "css-loader"],
-            template: path.resolve(__dirname, './build/styles.css')
+          
           },
         ],
     },
