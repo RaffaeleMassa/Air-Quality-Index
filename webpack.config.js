b88b85ffbc5d59222c9dcbfed7f036384d462197
RaffaeleMassa <76770497+RaffaeleMassa@users.noreply.github.com>
2021-03-22 16:27:50 +0100

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
+  const Dotenv = require('dotenv-webpack')
+
+  const path = require('path')
+
+ 
+  module.exports = (env, argv) => {
+
+    const entryPath = argv.mode === 'development' ? './src/index_dev.js' : './src/index.js'
+    return {
+      entry: {
+        
+        main: path.resolve(__dirname, entryPath),
+      },
+      output: {
+        
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'build')
+      },
+      module: {
+        rules: [
+          {
+            test: /\.css$/,
+            use: ["style-loader", "css-loader"],
+          },
+        ],
+    },
+
+    
+      devServer: {
+        contentBase: './build',
+        open: true
+      },
+      plugins: [
+      
+        new HtmlWebpackPlugin({
+          title: "Pollution Forecast",
+          template: path.resolve(__dirname, './src/index.html'),
+        }),
+        
+        new Dotenv()
+      ]
+    }
+};
